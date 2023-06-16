d53411c981570f8215051f99c997e6a00e4b1c46
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-06-16 12:45:08 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AprendiendoAsp.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
